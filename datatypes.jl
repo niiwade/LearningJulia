9e195b23771f353fbc145f5b09b13251609e12b9
niiwade <niilantewade77@gmail.com>
2022-01-03 14:47:03 +0000

    datatypes & functions definition in Julia

---
+++ datatypes.jl
@@ -0,0 +1,51 @@
+using Printf
+using Statistics
+
+## boolean
+
+CanDrive = true
+
+CantDrive = false
+
+## integer
+
+# Int8 : -128 -127
+# Int16 : -32768 -32767
+# Int32 : -2147483648 -2147483647
+# Int64: -9223372036854775808 -9223372036854775807
+
+## float
+# Float32 : -3.402823e+38 -3.402823e+38
+# Float64 : -1.7976931348623157e+308 -1.7976931348623157e+308
+# UInt8 : 0 -255
+# UInt16 : 0 -65535
+
+
+## Julia lets you know the type of a variable out of the box
+my_answer =  23
+typeof(my_answer)
+
+my_pi = 3.141592653589793
+typeof(my_pi)
+
+mame = "Maye"
+typeof(mame)
+
+println(my_answer)
+println(my_pi)
+println(mame)
+
+
+#Syntax for basic Maths
+
+sum = 3 + 3
+
+difference = 10 - 3
+
+product = 3 * 3
+
+quotient = 10 / 3
+
+power = 2 ^ 3
+
+modulus = 10 % 3
--- variables.jl
+++ variables.jl
@@ -3,6 +3,9 @@ using Printf
 using Statistics
 
 ## types are dynamically assigned so its easier to use and change a variable
+## we dont have to explicitly state a variable's type , Julia automically figures it out
 
 s = 2
-print(s)
+s =  "Hello World"
+
+println(s)
